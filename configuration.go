ffc4e20319c17ecb2736454d7a7edf45d1405042
Support <Support@ClickSend.com>
2026-07-24 03:56:31 +0000

    ClickSend Go SDK v6.0.0

--- configuration.go
+++ configuration.go
@@ -89,7 +89,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "OpenAPI-Generator/1.0.0/go",
+		UserAgent:        "ClickSend-SDK/6.0.0/go",
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{
